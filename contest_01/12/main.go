3fbd6b828c432bdbd0e601605cc216d77e9d9fc3
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:53:28 +0300

    Update main.go

--- contest_01/12/main.go
+++ contest_01/12/main.go
@@ -1,1 +1,15 @@
-1
+package main
+import "fmt"
+
+func main() {
+    var x, g int
+    fmt.Scanf("%d", &x)
+    for i:=0; i!=1;i++{
+        if x == 1 {break}
+        if x % 2 != 0 {x = x*3 + 1
+        }else if x % 2 == 0 {x = x / 2}
+        g++
+        i=x
+    }
+    fmt.Println(g)
+}
