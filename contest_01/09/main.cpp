dbe4f931dbaa98a3ea2cd0aa73d3611532989705
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:52:05 +0300

    Update main.cpp

--- contest_01/09/main.cpp
+++ contest_01/09/main.cpp
@@ -1,1 +1,13 @@
-1
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a, b, c;
+    cin >> a;
+    cin >> b;
+    c = sqrt(a*a + b*b);
+    cout << c;
+}
