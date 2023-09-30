99c0c6b4f9228a0a3ab3c69d3262e0d792a5da89
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:53:07 +0300

    Update main.cpp

--- contest_01/11/main.cpp
+++ contest_01/11/main.cpp
@@ -1,1 +1,23 @@
-1
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int A, B, C;
+    cin >> A >> B >> C;
+    if (A > B && A > C) {
+        if (B > C) { cout << "B " << abs(A-B); }
+        else { cout << "C " << abs(A-C); }
+    }
+    if (A < B && A < C) {
+        if (B < C) { cout << "B " << abs(A-B); }
+        else { cout << "C " << abs(A-C); }
+    }
+    if ((A > B && A < C) || (A < B && A > C)) {
+        if (abs(A - B) < abs(A - C)) { cout << "B " << abs(A-B); }
+        else { cout << "C " << abs(A-C); }
+        }
+
+    }
