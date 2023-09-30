f59facdab59c55f61d1b335b0a909f8230d8d3f0
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:56:15 +0300

    Update main.cpp

--- contest_01/19/main.cpp
+++ contest_01/19/main.cpp
@@ -1,1 +1,39 @@
-1
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	string text;
+	getline(cin, text);
+	int len = text.length();
+	int posend = text.find("end");
+	int g = len - posend;
+	text.erase(posend, g);
+	stringstream ss(text);
+	string word;
+	set<string> res;
+
+	while (ss >> word) {
+		int d = 0;
+		int pos = text.find(word);
+		while (pos != string::npos) {
+			d++;
+			pos = text.find(word, pos + 1);
+		}
+		if (word != "end") {
+			if (d > 1)
+				res.insert(word);
+		}
+	}
+
+	for (string s : res) {
+		cout << s << " ";
+	}
+
+
+	return 0;
+}
