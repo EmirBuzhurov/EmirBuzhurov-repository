e267793e1d9c4ec224c263c35c917af8d01aa023
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:55:21 +0300

    Update main.cpp

--- contest_01/17/main.cpp
+++ contest_01/17/main.cpp
@@ -1,1 +1,35 @@
-1
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	long int n, d, res;
+	res = 0;
+	cin >> n;
+	std::vector<long int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	int left = 0, right = n - 1;
+	long int level = 0;
+
+	while (left < right) {
+		if (a[left] < a[right]) {
+			level = max(level, a[left]);
+			res += level - a[left];
+			left++;
+		}
+		else {
+			level = max(level, a[right]);
+			res += level - a[right];
+			right--;
+		}
+	}
+	cout << res;
+
+
+	return 0;
+}
+
