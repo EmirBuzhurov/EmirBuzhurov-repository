d494d644cb9a8858f785f3e65b5da12ee01865e9
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:54:49 +0300

    Update main.cpp

--- contest_01/15/main.cpp
+++ contest_01/15/main.cpp
@@ -1,1 +1,27 @@
-1
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string s1;
+    string s2;
+    int count = 1;
+    char a;
+    cin >> s1;
+    for (int i = 0;i < s1.length();i++) {
+        a = s1[i];
+        if (s1[i] == s1[i+1]) {
+                count++;
+        }
+        else if (s1[i]!=s1[i+1]) {
+            s2+=a;
+            if (count != 1 ) {s2+=to_string(count);}
+            count=1;
+        }
+    }
+    cout << s2;
+}
+
