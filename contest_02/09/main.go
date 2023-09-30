8a7c0df7b79cfdf634b653132dd9eacf74b1ae08
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 23:05:21 +0300

    Create main.go

---
+++ contest_02/09/main.go
@@ -0,0 +1,27 @@
+import "strconv"
+
+func isLucky(n string) bool {
+ if len(n) != 6 {
+  return false
+ }
+ sum1 := 0
+ sum2 := 0
+ for i := 0; i < len(n)/2; i++ {
+  n, err := strconv.Atoi(string(n[i]))
+  if err != nil {
+   return false
+  }
+  sum1 += n
+ }
+ for i := len(n) / 2; i < len(n); i++ {
+  n, err := strconv.Atoi(string(n[i]))
+  if err != nil {
+   return false
+  }
+  sum2 += n
+ }
+ if sum1 == sum2 {
+  return true
+ }
+ return false
+}
