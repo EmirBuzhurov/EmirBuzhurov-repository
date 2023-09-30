af525385fcdd6ae9d29cff6a9493bf882d17684e
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:55:59 +0300

    Update main.go

--- contest_01/18/main.go
+++ contest_01/18/main.go
@@ -1,1 +1,654 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	int A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, a1, a2, b2, c2, d2, e2, f2, g2, h2, a3, b3, c3, d3, e3, f3;
+	a2 = -1;
+	b2 = -1;
+	c2 = -1;
+	d2 = -1;
+	e2 = -1;
+	f2 = -1;
+	g2 = -1;
+	a3 = -1;
+	b3 = -1;
+	c3 = -1;
+	d3 = -1;
+	e3 = -1;
+	f3 = -1;
+	A = 1;
+	B = 2;
+	C = 3;
+	D = 4;
+	E = 5;
+	F = 6;
+	G = 7;
+	H = 8;
+	I = 9;
+	J = 10;
+	K = 11;
+	L = 12;
+	M = 13;
+	N = 14;
+	O = 15;
+	P = 16;
+	Q = 17;
+	R = 18;
+	S = 19;
+	T = 20;
+	U = 21;
+	V = 22;
+	W = 23;
+	X = 24;
+	Y = 25;
+	Z = 0;
+	cin >>a1;
+	if (a1<1 || a1>(2147483646)) {
+		return 0;
+	}
+	else {
+		if (a1 > 26) {
+			a2 = a1 % 26;
+			a3 = a1 / 26;
+		}
+		else a2 = a1;
+		if (a3 > 27) {
+			b2 = a3 % 26;
+			b3 = a3 / 26;
+		}
+		else {
+			if (a2 != 0 && a2 != -1) b2 = a3;
+			if (a2 == 0 && a3 != 1) b2 = a3 - 1;
+		}
+		if (b3 > 27) {
+			c2 = b3 % 26;
+			c3 = b3 / 26;
+		}
+		else {
+			if (b2 != 0 && b2 != -1)c2 = b3;
+			if (b2 == 0 && b3 != 1) c2 = b3 - 1;
+		}
+		if (c3 > 27) {
+			d2 = c3 % 26;
+			d3 = c3 / 26;
+		}
+		else {
+			if (c2 != 0 && c2 != -1)d2 = c3;
+			if (c2 == 0 && c3 != 1) d2 = c3 - 1;
+		}
+		if (d3 > 27) {
+			e2 = d3 % 26;
+			e3 = d3 / 26;
+		}
+		else {
+			if (d2 != 0 && d2 != -1)e2 = d3;
+			if (d2 == 0 && d3 != 1) e2 = d3 - 1;
+		}
+		if (e3 > 27) {
+			f2 = e3 % 26;
+			f3 = e3 / 26;
+		}
+		else {
+			if (e2 != 0 && e2 != -1)f2 = e3;
+			if (e2 == 0 && e3 != 1) f2 = e3 - 1;
+		}
+		if (f3 > 27) {
+			g2 = f3 % 26;
+		}
+		else {
+			if (f2 != 0 && f2 != -1)g2 = f3;
+			if (f2 == 0 && f3 != 1) g2 = f3 - 1;
+		}
+		if (g2 == A) {
+			cout << "A";
+		}
+		else if (g2 == B) {
+			cout << "B";
+		}
+		else if (g2 == C) {
+			cout << "C";
+		}
+		else if (g2 == D) {
+			cout << "D";
+		}
+		else if (g2 == E) {
+			cout << "E";
+		}
+		else if (g2 == F) {
+			cout << "F";
+		}
+		else if (g2 == G) {
+			cout << "G";
+		}
+		else if (g2 == H) {
+			cout << "H";
+		}
+		else if (g2 == I) {
+			cout << "I";
+		}
+		else if (g2 == J) {
+			cout << "J";
+		}
+		else if (g2 == K) {
+			cout << "K";
+		}
+		else if (g2 == L) {
+			cout << "L";
+		}
+		else if (g2 == M) {
+			cout << "M";
+		}
+		else if (g2 == N) {
+			cout << "N";
+		}
+		else if (g2 == O) {
+			cout << "O";
+		}
+		else if (g2 == P) {
+			cout << "P";
+		}
+		else if (g2 == Q) {
+			cout << "Q";
+		}
+		else if (g2 == R) {
+			cout << "R";
+		}
+		else if (g2 == S) {
+			cout << "S";
+		}
+		else if (g2 == T) {
+			cout << "T";
+		}
+		else if (g2 == U) {
+			cout << "U";
+		}
+		else if (g2 == V) {
+			cout << "V";
+		}
+		else if (g2 == W) {
+			cout << "W";
+		}
+		else if (g2 == X) {
+			cout << "X";
+		}
+		else if (g2 == Y) {
+			cout << "Y";
+		}
+		else if (g2 == Z || g2 == 26) {
+			cout << "Z";
+		}
+		if (f2 == A) {
+			cout << "A";
+		}
+		else if (f2 == B) {
+			cout << "B";
+		}
+		else if (f2 == C) {
+			cout << "C";
+		}
+		else if (f2 == D) {
+			cout << "D";
+		}
+		else if (f2 == E) {
+			cout << "E";
+		}
+		else if (f2 == F) {
+			cout << "F";
+		}
+		else if (f2 == G) {
+			cout << "G";
+		}
+		else if (f2 == H) {
+			cout << "H";
+		}
+		else if (f2 == I) {
+			cout << "I";
+		}
+		else if (f2 == J) {
+			cout << "J";
+		}
+		else if (f2 == K) {
+			cout << "K";
+		}
+		else if (f2 == L) {
+			cout << "L";
+		}
+		else if (f2 == M) {
+			cout << "M";
+		}
+		else if (f2 == N) {
+			cout << "N";
+		}
+		else if (f2 == O) {
+			cout << "O";
+		}
+		else if (f2 == P) {
+			cout << "P";
+		}
+		else if (f2 == Q) {
+			cout << "Q";
+		}
+		else if (f2 == R) {
+			cout << "R";
+		}
+		else if (f2 == S) {
+			cout << "S";
+		}
+		else if (f2 == T) {
+			cout << "T";
+		}
+		else if (f2 == U) {
+			cout << "U";
+		}
+		else if (f2 == V) {
+			cout << "V";
+		}
+		else if (f2 == W) {
+			cout << "W";
+		}
+		else if (f2 == X) {
+			cout << "X";
+		}
+		else if (f2 == Y) {
+			cout << "Y";
+		}
+		else if (f2 == Z || f2 == 26) {
+			cout << "Z";
+		}
+		if (e2 == A) {
+			cout << "A";
+		}
+		else if (e2 == B) {
+			cout << "B";
+		}
+		else if (e2 == C) {
+			cout << "C";
+		}
+		else if (e2 == D) {
+			cout << "D";
+		}
+		else if (e2 == E) {
+			cout << "E";
+		}
+		else if (e2 == F) {
+			cout << "F";
+		}
+		else if (e2 == G) {
+			cout << "G";
+		}
+		else if (e2 == H) {
+			cout << "H";
+		}
+		else if (e2 == I) {
+			cout << "I";
+		}
+		else if (e2 == J) {
+			cout << "J";
+		}
+		else if (e2 == K) {
+			cout << "K";
+		}
+		else if (e2 == L) {
+			cout << "L";
+		}
+		else if (e2 == M) {
+			cout << "M";
+		}
+		else if (e2 == N) {
+			cout << "N";
+		}
+		else if (e2 == O) {
+			cout << "O";
+		}
+		else if (e2 == P) {
+			cout << "P";
+		}
+		else if (e2 == Q) {
+			cout << "Q";
+		}
+		else if (e2 == R) {
+			cout << "R";
+		}
+		else if (e2 == S) {
+			cout << "S";
+		}
+		else if (e2 == T) {
+			cout << "T";
+		}
+		else if (e2 == U) {
+			cout << "U";
+		}
+		else if (e2 == V) {
+			cout << "V";
+		}
+		else if (e2 == W) {
+			cout << "W";
+		}
+		else if (e2 == X) {
+			cout << "X";
+		}
+		else if (e2 == Y) {
+			cout << "Y";
+		}
+		else if (e2 == Z || e2 == 26) {
+			cout << "Z";
+		}
+		if (d2 == A) {
+			cout << "A";
+		}
+		else if (d2 == B) {
+			cout << "B";
+		}
+		else if (d2 == C) {
+			cout << "C";
+		}
+		else if (d2 == D) {
+			cout << "D";
+		}
+		else if (d2 == E) {
+			cout << "E";
+		}
+		else if (d2 == F) {
+			cout << "F";
+		}
+		else if (d2 == G) {
+			cout << "G";
+		}
+		else if (d2 == H) {
+			cout << "H";
+		}
+		else if (d2 == I) {
+			cout << "I";
+		}
+		else if (d2 == J) {
+			cout << "J";
+		}
+		else if (d2 == K) {
+			cout << "K";
+		}
+		else if (d2 == L) {
+			cout << "L";
+		}
+		else if (d2 == M) {
+			cout << "M";
+		}
+		else if (d2 == N) {
+			cout << "N";
+		}
+		else if (d2 == O) {
+			cout << "O";
+		}
+		else if (d2 == P) {
+			cout << "P";
+		}
+		else if (d2 == Q) {
+			cout << "Q";
+		}
+		else if (d2 == R) {
+			cout << "R";
+		}
+		else if (d2 == S) {
+			cout << "S";
+		}
+		else if (d2 == T) {
+			cout << "T";
+		}
+		else if (d2 == U) {
+			cout << "U";
+		}
+		else if (d2 == V) {
+			cout << "V";
+		}
+		else if (d2 == W) {
+			cout << "W";
+		}
+		else if (d2 == X) {
+			cout << "X";
+		}
+		else if (d2 == Y) {
+			cout << "Y";
+		}
+		else if (d2 == Z || d2 == 26) {
+			cout << "Z";
+		}
+		if (c2 == A) {
+			cout << "A";
+		}
+		else if (c2 == B) {
+			cout << "B";
+		}
+		else if (c2 == C) {
+			cout << "C";
+		}
+		else if (c2 == D) {
+			cout << "D";
+		}
+		else if (c2 == E) {
+			cout << "E";
+		}
+		else if (c2 == F) {
+			cout << "F";
+		}
+		else if (c2 == G) {
+			cout << "G";
+		}
+		else if (c2 == H) {
+			cout << "H";
+		}
+		else if (c2 == I) {
+			cout << "I";
+		}
+		else if (c2 == J) {
+			cout << "J";
+		}
+		else if (c2 == K) {
+			cout << "K";
+		}
+		else if (c2 == L) {
+			cout << "L";
+		}
+		else if (c2 == M) {
+			cout << "M";
+		}
+		else if (c2 == N) {
+			cout << "N";
+		}
+		else if (c2 == O) {
+			cout << "O";
+		}
+		else if (c2 == P) {
+			cout << "P";
+		}
+		else if (c2 == Q) {
+			cout << "Q";
+		}
+		else if (c2 == R) {
+			cout << "R";
+		}
+		else if (c2 == S) {
+			cout << "S";
+		}
+		else if (c2 == T) {
+			cout << "T";
+		}
+		else if (c2 == U) {
+			cout << "U";
+		}
+		else if (c2 == V) {
+			cout << "V";
+		}
+		else if (c2 == W) {
+			cout << "W";
+		}
+		else if (c2 == X) {
+			cout << "X";
+		}
+		else if (c2 == Y) {
+			cout << "Y";
+		}
+		else if (c2 == Z || c2 == 26) {
+			cout << "Z";
+		}
+		if (b2 == A) {
+			cout << "A";
+		}
+		else if (b2 == B) {
+			cout << "B";
+		}
+		else if (b2 == C) {
+			cout << "C";
+		}
+		else if (b2 == D) {
+			cout << "D";
+		}
+		else if (b2 == E) {
+			cout << "E";
+		}
+		else if (b2 == F) {
+			cout << "F";
+		}
+		else if (b2 == G) {
+			cout << "G";
+		}
+		else if (b2 == H) {
+			cout << "H";
+		}
+		else if (b2 == I) {
+			cout << "I";
+		}
+		else if (b2 == J) {
+			cout << "J";
+		}
+		else if (b2 == K) {
+			cout << "K";
+		}
+		else if (b2 == L) {
+			cout << "L";
+		}
+		else if (b2 == M) {
+			cout << "M";
+		}
+		else if (b2 == N) {
+			cout << "N";
+		}
+		else if (b2 == O) {
+			cout << "O";
+		}
+		else if (b2 == P) {
+			cout << "P";
+		}
+		else if (b2 == Q) {
+			cout << "Q";
+		}
+		else if (b2 == R) {
+			cout << "R";
+		}
+		else if (b2 == S) {
+			cout << "S";
+		}
+		else if (b2 == T) {
+			cout << "T";
+		}
+		else if (b2 == U) {
+			cout << "U";
+		}
+		else if (b2 == V) {
+			cout << "V";
+		}
+		else if (b2 == W) {
+			cout << "W";
+		}
+		else if (b2 == X) {
+			cout << "X";
+		}
+		else if (b2 == Y) {
+			cout << "Y";
+		}
+		else if (b2 == Z || b2 == 26) {
+			cout << "Z";
+		}
+		if (a2 == A) {
+			cout << "A";
+		}
+		else if (a2 == B) {
+			cout << "B";
+		}
+		else if (a2 == C) {
+			cout << "C";
+		}
+		else if (a2 == D) {
+			cout << "D";
+		}
+		else if (a2 == E) {
+			cout << "E";
+		}
+		else if (a2 == F) {
+			cout << "F";
+		}
+		else if (a2 == G) {
+			cout << "G";
+		}
+		else if (a2 == H) {
+			cout << "H";
+		}
+		else if (a2 == I) {
+			cout << "I";
+		}
+		else if (a2 == J) {
+			cout << "J";
+		}
+		else if (a2 == K) {
+			cout << "K";
+		}
+		else if (a2 == L) {
+			cout << "L";
+		}
+		else if (a2 == M) {
+			cout << "M";
+		}
+		else if (a2 == N) {
+			cout << "N";
+		}
+		else if (a2 == O) {
+			cout << "O";
+		}
+		else if (a2 == P) {
+			cout << "P";
+		}
+		else if (a2 == Q) {
+			cout << "Q";
+		}
+		else if (a2 == R) {
+			cout << "R";
+		}
+		else if (a2 == S) {
+			cout << "S";
+		}
+		else if (a2 == T) {
+			cout << "T";
+		}
+		else if (a2 == U) {
+			cout << "U";
+		}
+		else if (a2 == V) {
+			cout << "V";
+		}
+		else if (a2 == W) {
+			cout << "W";
+		}
+		else if (a2 == X) {
+			cout << "X";
+		}
+		else if (a2 == Y) {
+			cout << "Y";
+		}
+		else if (a2 == Z || a2 == 26) {
+			cout << "Z";
+		}
+	}
+
+
+	return 0;
+}
