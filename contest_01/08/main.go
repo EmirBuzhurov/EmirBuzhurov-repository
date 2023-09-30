31ec886038e5e188582c296cc5d69f0f8d0b9287
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:51:46 +0300

    Update main.go

--- contest_01/08/main.go
+++ contest_01/08/main.go
@@ -1,1 +1,23 @@
-1
+package main
+import "fmt"
+
+func main() {
+    var a, b, c int
+    fmt.Scanf("%d %d %d", &a, &b, &c);
+    if a > b {
+        if a > c {
+            fmt.Println(a);
+        }
+        if a < c {
+            fmt.Println(c);
+        }
+    }
+    if a < b {
+        if b > c {
+           fmt.Println(b); 
+        }
+        if b < c {
+            fmt.Println(c);
+        }
+    }
+}
