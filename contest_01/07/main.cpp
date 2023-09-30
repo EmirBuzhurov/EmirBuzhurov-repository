514a3c7ed595d26fa411d031af31d27c7a4e89bc
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:51:07 +0300

    Update main.cpp

--- contest_01/07/main.cpp
+++ contest_01/07/main.cpp
@@ -1,1 +1,12 @@
-1
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    cin >> a >> b;
+    if (a > b) { cout << a; }
+    else { cout << b; }
+}
