4c1f7f918572252480431189926f64148b78b08c
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:56:34 +0300

    Update main.go

--- contest_01/20/main.go
+++ contest_01/20/main.go
@@ -1,1 +1,23 @@
-1
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    int i = 0;
+    sort(begin(a), end(a));
+    sort(begin(b), end(b));
+    for (i; i < a.length(); i++) {
+        if (a[i] != b[i]){
+            cout << "NO";
+            break;
+        }
+    }
+    if (i == a.length()) {
+        cout << "YES";
+    }
+}
