560e0aca3f841fe93e93ce9e93ecc82ab579ef55
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-09-30 22:50:46 +0300

    Update main.go

--- contest_01/06/main.go
+++ contest_01/06/main.go
@@ -1,1 +1,29 @@
-1
+package main
+import "fmt"
+
+func main() {
+    var a, b, c, d, e, f, g int
+    fmt.Scanf("%d\n",&f);
+    g = f
+    if f>=5000{
+        a = f / 5000
+        g = f%5000
+    }
+    if g>=1000{
+        b = g / 1000
+        g = g%1000
+    }
+    if g>=500{
+        c = g / 500
+        g = g%500
+    }
+    if g>=200{
+        d = g / 200
+        g = g%200
+    }
+    if g>=100{
+        e = g / 100
+        g = g%100
+    }
+   fmt.Println(a,b,c,d,e)
+}
