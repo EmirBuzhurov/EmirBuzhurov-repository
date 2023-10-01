770ecd171f688c380ec982b07ba99a3713bfffb6
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-01 23:03:34 +0300

    Update main.cpp

--- contest_02/03/main.cpp
+++ contest_02/03/main.cpp
@@ -1,1 +1,12 @@
-1
+bool compare(const std::string& a, const std::string& b){
+    int countA = std::count(a.begin(), a.end(), '1');
+    int countB = std::count(b.begin(), b.end(), '1');
+    if(countA != countB){
+        return countA > countB;
+    }
+    else{
+        int n{ std::stoi(a) };
+        int m{ std::stoi(b) };
+        return n < m;
+    }
+}
