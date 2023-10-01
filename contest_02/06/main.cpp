99ad412c1f601ba3435152c2f96c1509788d1b5f
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-01 23:05:25 +0300

    Update main.cpp

--- contest_02/06/main.cpp
+++ contest_02/06/main.cpp
@@ -4,7 +4,6 @@ Student make_student(const std::string& line) {
     std::string group = line.substr(delim_pos + 1);
     return {name, group};
 }
-
 bool is_upper(const Student& s1, const Student& s2) {
     if (s1.group < s2.group) {
         return true;
@@ -14,13 +13,11 @@ bool is_upper(const Student& s1, const Student& s2) {
         return s1.name < s2.name;
     }
 }
-
 void print(const std::vector<Student>& students) {
     if (students.empty()) {
         std::cout << "Empty list!\n";
         return;
     }
-    
     std::string current_group = students[0].group;
     std::cout << current_group << "\n";
     for (const Student& student : students) {
