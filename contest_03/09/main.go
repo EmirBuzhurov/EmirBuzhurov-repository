d0cdfabf328d8ee97d52dfeb87557521d3d34e8d
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-17 10:10:05 +0300

    Update main.go

--- contest_03/09/main.go
+++ contest_03/09/main.go
@@ -1,1 +1,31 @@
-1
+package main
+import (
+    "fmt"
+    "sort"
+    )
+
+func main() {
+    var n,x int;
+    fmt.Scanln(&n, &x)
+    num:=make ([] int,n)
+    for i:=0;i<n;i++{
+        fmt.Scan(&num[i])
+    }
+    sort.Ints(num)
+    left:=0
+    right:=n-1
+    for left<right{
+        sum:= num[left]+num[right]
+        if sum==x{
+            fmt.Print(num[left],num[right])
+            break
+        }else if sum<x{
+            left++
+        }else {
+            right--
+        } 
+    }
+    if left==right{
+        fmt.Print("0 ","0")
+    }
+}
