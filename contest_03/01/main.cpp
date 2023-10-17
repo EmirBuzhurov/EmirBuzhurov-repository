fd3afce6a3fcec6f54aaca622181cb3f1220154a
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-17 09:56:59 +0300

    Update main.cpp

--- contest_03/01/main.cpp
+++ contest_03/01/main.cpp
@@ -1,1 +1,35 @@
-1
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int a, b, d=0;
+    cin >> a;
+    string* s1{ new string[a] };
+    for (int i=0; i < a; i++) {
+        cin >> s1[i];
+    }
+    cin >> b;
+    string* s2{ new string[b] };
+    for (int i=0; i < b; i++) {
+        cin >> s2[i];
+    }
+    int c = a + b;
+    int* num{ new int[c] };
+    for (int i = 0; i < a; i++) {
+        num[i] = stoi(s1[i]);
+    }
+    for (int i = a; i < c; i++) {
+        num[i] = stoi(s2[d]);
+        d++;
+    }
+    sort(num, num + c);
+    for (int i = 0; i < c; i++) {
+        cout << num[i];
+        if (i != c - 1) {
+            cout << " ";
+        }
+    }
+}
