1cac44efb0dfc990fbfb2a3f6e3a05b2546afb09
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-17 09:58:05 +0300

    Update main.cpp

--- contest_03/02/main.cpp
+++ contest_03/02/main.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+bool isBalanced(const string& text) {
+    stack<char> brackets;
+    for (char ch : text) {
+        if (ch == '(' || ch == '{' || ch == '[') {
+            brackets.push(ch);
+        }
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (brackets.empty()) {
+                return false;
+            }
+            char openBracket = brackets.top();
+            brackets.pop();
+            if ((ch == ')' && openBracket != '(') ||
+                (ch == '}' && openBracket != '{') ||
+                (ch == ']' && openBracket != '[')) {
+                return false;
+            }
+        }
+    }
+    return brackets.empty();
+}
+int main() {
+    string text;
+    getline(cin, text, '!');
 
+    if (isBalanced(text)) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
