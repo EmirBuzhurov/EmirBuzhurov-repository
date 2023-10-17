9a5a583a3c169098276e6bfeb796f02e005b334d
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-17 10:09:33 +0300

    Update main.go

--- contest_03/08/main.go
+++ contest_03/08/main.go
@@ -1,1 +1,14 @@
+package main
+import "fmt"
 
+func main() {
+    var n int
+    fmt.Scan(&n)
+    var result int
+    for i := 0; i < n; i++ {
+        var num int
+        fmt.Scan(&num)
+        result ^= num
+    }
+    fmt.Println(result)
+}
