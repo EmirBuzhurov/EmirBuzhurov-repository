a6fc1c3d078b588a7ed4130cc81bf7cfc364e781
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-17 10:04:45 +0300

    Update main.cpp

--- contest_03/06/main.cpp
+++ contest_03/06/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <map>
 
+int main() {
+    std::map<std::string, int> data;
+    int userCount;
+    std::cin >> userCount;
+    std::cin.ignore(1);
+    for (int i = 0; i < userCount; i++) {
+        std::string login;
+        std::getline(std::cin, login, ';');
+        int money;
+        std::cin >> money;
+        std::cin.ignore(1);
+        data[login] = money;
+    }
+    int reqCount;
+    std::cin >> reqCount;
+    for (int i = 0; i < reqCount; i++) {
+        std::string login;
+        std::cin >> login;
+        std::cout << data[login] << " ";
+    }
+    std::cout << "\n";
+}
