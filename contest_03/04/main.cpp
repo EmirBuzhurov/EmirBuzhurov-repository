a793abf354d41f0c62e79b37b4f943d54b6a855f
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-17 10:02:56 +0300

    Update main.cpp

--- contest_03/04/main.cpp
+++ contest_03/04/main.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <sstream>
+#include <set>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+int main() {
+    int n, m;
+    cin >> n;
+    cin.ignore();
+    set<string> words1;
+    string word;
+    for (int i = 0; i < n; ++i) {
+        cin >> word;
+        words1.insert(word);
+    }
+    cin >> m;
+    cin.ignore();
+    set<string> words2;
+    for (int i = 0; i < m; ++i) {
+        cin >> word;
+        words2.insert(word);
+    }
+    set<string> common_words;
+    set_intersection(words1.begin(), words1.end(), words2.begin(), words2.end(),
+                          inserter(common_words, common_words.begin()));
+    if (common_words.empty()) 
+        cout << -1;
+    else {
+        for (const auto& word : common_words){
+            cout << word << " ";
+        }
+    }
+}
