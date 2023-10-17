9534b5de67f0c789bde87da227e6c908d94064d0
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-17 10:06:44 +0300

    Update main.cpp

--- contest_03/07/main.cpp
+++ contest_03/07/main.cpp
@@ -1,1 +1,45 @@
-1
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+    ifstream file("data.txt");
+    string target;
+    cin >> target;
+    map<string, int> answers;
+    string temp;
+    file >> temp;
+    while (temp != "stopword") {
+        if (temp == target) {
+            file >> temp;
+            if (temp == "stopword") {
+                break;
+            }
+            answers[temp]++;
+        }
+        file >> temp;
+    }
+    if (answers.empty()) {
+        cout << "-" << "\n";
+        return 0;
+    }
+    map<int, set<string>> final;
+    for (auto& [word, count] : answers) {
+        final[count].insert(word);
+    }
+    int count = 0;
+    for (auto it = final.rbegin(); it != final.rend(); ++it) {
+        for (auto word : it->second) {
+            cout << word << " ";
+            if (++count >= 3) {
+                break;
+            }
+        }
+        if (count >= 3) {
+            break;
+        }
+    }
+    cout << "\n";
+}
