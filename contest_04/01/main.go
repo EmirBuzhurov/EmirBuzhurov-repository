3d3ac8ea3890692cf619611fa8c42ca21ce013e3
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-31 10:58:41 +0300

    Update main.go

--- contest_04/01/main.go
+++ contest_04/01/main.go
@@ -1,1 +1,41 @@
-1
+package main
+import (
+  "encoding/json"
+  "fmt"
+  "io"
+  "os"
+)
+type Project struct {
+  ProjectID int `json:"project_id"`
+  Tasks []Task `json:"tasks"`
+}
+
+type Task struct {
+  UserID int    `json:"user_id"`
+  ID int    `json:"id"`
+  Title string `json:"title"`
+  Completed bool `json:"completed"`
+}
+
+func main() {
+  var userID int
+  fmt.Scan(&userID)
+  file, _ := os.Open("data.json")
+  defer file.Close()
+  data, _ := io.ReadAll(file)
+  var projects []Project
+  err := json.Unmarshal(data, &projects)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  completedTasks := 0
+  for _, project := range projects {
+    for _, task := range project.Tasks {
+      if task.UserID == userID && task.Completed {
+        completedTasks++
+      }
+    }
+  }
+  fmt.Println(completedTasks)
+}
