2e30f2f7862fe8543b7da913682d101426290fd3
EmirBuzhurov <144903013+EmirBuzhurov@users.noreply.github.com>
2023-10-31 10:59:15 +0300

    Update main.cpp

--- contest_04/02/main.cpp
+++ contest_04/02/main.cpp
@@ -1,1 +1,22 @@
-1
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+int main()
+{
+    ifstream file("data.json");
+    json data = json::parse(file);
+    int user_id, counter = 0;
+    cin >> user_id;
+    for(const auto&i:data) {
+        for(const auto&j:i["tasks"]) {
+            if (j["user_id"]==user_id && j["completed"]==true) {
+                counter++;
+            }
+        }
+    }
+    cout << counter;
+}
